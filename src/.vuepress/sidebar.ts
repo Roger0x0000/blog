3db7efd12c8d9df68d98a2942cb5be36c8e412e0
Roger Huang <Roger.Huang2@flex.com>
2025-07-02 13:54:20 +0800

    1.更新博客模板

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -16,36 +16,41 @@ export default sidebar({
         prefix: "posts/",
         children: [
             {
-                text: "苹果",
+                text: "Buck",
                 icon: "gear",
-                prefix: "apple/",
+                prefix: "Buck/",
                 collapsible: true,
                 children: "structure"
             },
             {
-                text: "香蕉",
+                text: "Buck-Boost",
                 icon: "gear",
-                prefix: "banana/",
+                prefix: "BuckBoost/",
                 collapsible: true,
                 children: "structure"
             },
         ],
     },
     {
-        text: "项目 projects",
+        text: "数字电源开发板",
         icon: "star",
         collapsible: true,
         expanded: true,
         children: [
             {
-                text: "弦月档案",
+                text: "Buck",
                 icon: "link",
-                link: "https://twis.uk/yisibite",
+                link: "https://item.taobao.com/item.htm?id=812182316855&ltk2=1751435471197wfzjgiaj81600lyf22xqq&sku_properties=5919063%3A14198304&spm=a21xtw.29178619.0.0",
             },
             {
-                text: "线代笔记",
+                text: "Buck-Boost",
                 icon: "link",
-                link: "https://twis.uk/Notes-on-Linear-Algebra"
+                link: "https://item.taobao.com/item.htm?abbucket=18&detail_redpacket_pop=true&id=940186646234&ltk2=1751435426855x72w7bbpen0w1jigi6rpmq&ns=1&priceTId=2147849317514354167707212e1bef&query=%E6%95%B0%E5%AD%97%E7%94%B5%E6%BA%90%E5%BC%80%E5%8F%91%E6%9D%BF&skuId=6011490299281&spm=a21n57.1.hoverItem.9&utparam=%7B%22aplus_abtest%22%3A%22e78fcf69a3173bb583c01ff68042f646%22%7D&xxc=taobaoSearch"
+            },
+            {
+                text: "PFC",
+                icon: "link",
+                link: "https://item.taobao.com/item.htm?id=920530458957&ltk2=1751435488322k3i9k6zpaxrajon8nkfy7n&sku_properties=5919063%3A14198304&spm=a21xtw.29178619.0.0",
             }
         ],
     }
